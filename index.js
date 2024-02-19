aac6b33f90f7955ab2b16911bc80ee4a080d4817
Eugene Esperas <eugene.esperas05@gmail.com>
2024-02-20 00:22:07 +0800

    portfolio upload v2

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 // portfolio
 
+const activeNav = document.querySelectorAll(".nav-link");
 
+activeNav.forEach((nav) => {
+  nav.addEventListener("click", () => {
+    activeNav.forEach((active) => {
+      active.removeAttribute("id");
+    });
 
+    nav.setAttribute("id", "active-nav");
+  });
+});
